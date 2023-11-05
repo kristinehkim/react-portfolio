ec2bbd1f7940b319efa5971ffba4d313b1c55c63
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-05 10:27:52 -0800

    worked on header and included icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/kristinekim.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Kristine Kim</h1>
+        <h5 className="text-light">Software Development Professional</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt='' />
+
+          <a href='#contact' className='scroll_down'>Scroll Down</a>
+        </div>
+      </div>
+      
+    </header>
   )
 }
 
