deffd4cb3bc561099a2623989fc141be6a2ccf1f
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-05 19:31:14 -0800

    fixed the layout of the about cards to be on the right of the image, the closing div of the about container was in the wrong spot

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,35 +17,35 @@ const About = () => {
           </div>
         </div>
 
-      </div>
 
-      <div className='about__content'>
-        <div className='about__cards'>
-          <article className='about__card'>
-            <FaAward className='about__icon' />
-            <h5>Experience</h5>
-            <small>10+ Years Working</small>
-          </article>
-          
-          <article className='about__card'>
-            <FaAward className='about__icon' />
-            <h5>Experience</h5>
-            <small>10+ Years Working</small>
-          </article>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>10+ Years Working</small>
+            </article>
 
-          <article className='about__card'>
-            <VscFolderLibrary className='projects__icon' />
-            <h5>Projects</h5>
-            <small>10+ Completed</small>
-          </article>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>10+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            My name is Kristine Kim and I was a teacher for 16 years. I taught English to first graders in Mexico for 4 years then moved back to San Diego. I was at an inner city school teaching 5th grade for 12 years. I incorporated technology into the classroom on a daily basis which led me to an interest in technology. I am now looking to get into the tech industry. Being a teacher has molded me into a dilligent worker that will transfer into any job in any industry. I have learned a huge amount of information through UCSD coding bootcamp program, and I am excited to showcase my skills in this portfolio. Please browse this site showcasing my abilities and what I have accomplished during the course. If interested in reaching out to me, go to the contact section by clicking on the link above and the LinkedIn, GitHub, and Twitter icons below will take you to my profiles.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
         </div>
-
-        <p>
-        My name is Kristine Kim and I was a teacher for 16 years. I taught English to first graders in Mexico for 4 years then moved back to San Diego. I was at an inner city school teaching 5th grade for 12 years. I incorporated technology into the classroom on a daily basis which led me to an interest in technology. I am now looking to get into the tech industry. Being a teacher has molded me into a dilligent worker that will transfer into any job in any industry. I have learned a huge amount of information through UCSD coding bootcamp program, and I am excited to showcase my skills in this portfolio. Please browse this site showcasing my abilities and what I have accomplished during the course. If interested in reaching out to me, go to the contact section by clicking on the link above and the LinkedIn, GitHub, and Twitter icons below will take you to my profiles.
-        </p>
-
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-
       </div>
     </section>
   )
