5e342a51b803e6bf39c94cc4e7485e9532b7ce75
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-05 17:29:26 -0800

    completed navbar/headersocials, about section added pic and styled

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BsCardChecklist} from 'react-icons/bs'
-import {AiOutlineFolderOpen} from 'react-icons/ai'
-import {BiMessageAltDetail} from 'react-icons/bi'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsCardChecklist } from 'react-icons/bs'
+import { AiOutlineFolderOpen } from 'react-icons/ai'
+import { BiMessageAltDetail } from 'react-icons/bi'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -12,11 +12,11 @@ const Nav = () => {
   return (
     <nav>
       {/* if activeNav is equal to # then we want to give it the active class, if not, we will give it nothing */}
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BsCardChecklist/></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageAltDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BsCardChecklist /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageAltDetail /></a>
     </nav>
   )
 }
