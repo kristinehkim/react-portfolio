5f2fef279077fa6ba9c183937f397f2124f8fe6f
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-08 19:55:29 -0800

    added portfolio images, links to github repo and demo in portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import DOGGYGPS from '../../assets/doggygps.png'
+import GITAJOB from '../../assets/gitajob.png'
+import EDURATER from '../../assets/edurater.png'
+import PASSWORD from '../../assets/passwordgenerator.png'
+import SCHEDULER from '../../assets/scheduler.png'
+import TEXT from '../../assets/texteditor.png'
+
+const data = [
+  {
+    id: 1,
+    image: DOGGYGPS,
+    title: 'Doggy GPS',
+    github: 'https://github.com/kristinehkim/doggygps',
+    demo: 'https://kristinehkim.github.io/doggygps/'
+  },
+  {
+    id: 2,
+    image: GITAJOB,
+    title: 'Git A Job',
+    github: 'https://github.com/kristinehkim/git-a-job',
+    demo: 'https://lit-dawn-72869-0437fa1dd280.herokuapp.com/login'
+  },
+  {
+    id: 3,
+    image: EDURATER,
+    title: 'EduRATEr',
+    github: 'https://github.com/kristinehkim/edurater',
+    demo: 'https://damp-wildwood-31060-690d212e58d9.herokuapp.com/'
+  },
+  {
+    id: 4,
+    image: PASSWORD,
+    title: 'Password Generator',
+    github: 'https://github.com/kristinehkim/password-generator',
+    demo: 'https://kristinehkim.github.io/password-generator/'
+  },
+  {
+    id: 5,
+    image: SCHEDULER,
+    title: 'Work Day Scheduler',
+    github: 'https://github.com/kristinehkim/work-day-scheduler',
+    demo: 'https://kristinehkim.github.io/work-day-scheduler/'
+  },
+  {
+    id: 6,
+    image: TEXT,
+    title: 'Text Editor',
+    github: 'https://github.com/kristinehkim/text-editor',
+    demo: 'https://ancient-basin-84021-467278b42de0.herokuapp.com/'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='blank'>GitHub Repo</a>
+                  <a href={demo} className='btn btn-primary' target='blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
