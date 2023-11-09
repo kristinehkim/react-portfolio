d5428934859736270adf0fbaadb3f72ddbaaef41
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-09 15:54:02 -0800

    changed to extras component, added swiper.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
+import Extras from './components/extras/Extras'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -16,7 +16,7 @@ const App = () => {
       <About />
       <Skills />
       <Portfolio />
-      <Testimonials />
+      <Extras />
       <Contact />
       <Footer />
 
---
+++ src/components/extras/Extras.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './extras.css'
+import GIRLS from '../../assets/girlsintechsf.png'
+import EXTRA1 from '../../assets/extra1.png'
+import EXTRA2 from '../../assets/extra2.png'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    image: GIRLS,
+    name: 'Hacking for Humanity',
+    review: 'Hacking for Humanity hackathon at MindsDB AI Collective in San Francisco.  I worked with a group of women to solve a problem.  We won ____ place!'
+  },
+  {
+    image: EXTRA1,
+    name: 'Extra 1',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam error ipsum expedita ipsam dolorem illum hic debitis voluptates dolorum, optio quisquam, eum recusandae ab temporibus perferendis, fugiat reiciendis sed fuga?'
+  },
+  {
+    image: EXTRA2,
+    name: 'Extra 2',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam error ipsum expedita ipsam dolorem illum hic debitis voluptates dolorum, optio quisquam, eum recusandae ab temporibus perferendis, fugiat reiciendis sed fuga?'
+  }
+]
+
+const Extras = () => {
+  return (
+    <section id='extras'>
+      <h5>Other</h5>
+      <h2>Extra Curricular</h2>
+
+      <Swiper className='container extras__container'// install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      onSlideChange={() => console.log('slide change')}
+    >
+        {
+          data.map(({ image, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className='extra'>
+                <div className='extra__img'>
+                  <img src={image} alt='Visual of extra'/>
+                </div>
+                <h5 className='extra__name'>{name}</h5>
+                <small className='extra__review'>{review}</small>
+              </SwiperSlide>
+
+            )
+          })
+        }
+      </Swiper>
+    </section>
+  )
+}
+
+export default Extras
