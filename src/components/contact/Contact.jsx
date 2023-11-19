42f30a3b54ce71ea26d8606777cbbc3e7316ebdf
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-11-18 16:07:38 -0800

    contact and footer completed with styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from "react-icons/md"
+import { BsLinkedin } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_aahpjyp', 'template_ql07vkp', form.current, 'JqBJ7F0mDIy6-vqRU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In ATouch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>kristinehkim@gmail.com</h5>
+            <a href='mailto:kristinehkim@gmail.com'target='blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin className='contact__option-icon' />
+            <h4>LinkedIn</h4>
+            <h5>kristinehkim@gmail.com</h5>
+            <a href='https://www.linkedin.com/in/kristinehkim/' target='blank'>Send a message</a>
+          </article>
+          {/* <article className='contact__option'>
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>kristinehkim@gmail.com</h5>
+            <a href='https://www.linkedin.com/in/kristinehkim/'>Send a message</a>
+          </article> */}
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import { FaGithub } from 'react-icons/fa'
+import { FaXTwitter } from "react-icons/fa6"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#header' className='footer__tag'>Kristine Kim</a>
+
+      <ul className='permalinks'>
+        <li><a href='#header'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#skills'>Skills</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://github.com/kristinehkim'><FaGithub /></a>
+        <a href='https://twitter.com/kristinehkim'><FaXTwitter /></a>
+      </div>
+
+      <div className='footer__copyright'>
+      <small>&copy; KRISTINE KIM. ALL RIGHTS RESERVED.</small>
+    </div>
+    </footer>
+
   )
 }
 
